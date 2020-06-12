c791e98effc0370168176e0266b10a6398932fda
Draycia <lonelyyordle@gmail.com>
2020-06-12 03:23:32 -0700

    Add Cooldowns to depend and permission to tpgh

--- src/main/java/net/draycia/teleports/commands/misc/TeleportGroupHereCommand.java
+++ src/main/java/net/draycia/teleports/commands/misc/TeleportGroupHereCommand.java
@@ -3,6 +3,7 @@ package net.draycia.teleports.commands.misc;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import io.papermc.lib.PaperLib;
 import net.draycia.teleports.Teleports;
@@ -11,6 +12,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 @CommandAlias("tpgh|tpghere|tpgrouphere")
+@CommandPermission("teleports.teleport.group")
 public class TeleportGroupHereCommand extends BaseCommand {
 
     private Teleports main;
